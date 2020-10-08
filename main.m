2563ba59e63adc8c6c7d6dd152cdd8c92f66e962
Arugakente <arugakente@gmail.com>
2020-10-08 14:05:15 +0200

    Support for true google matrix

--- main.m
+++ main.m
@@ -1,17 +1,19 @@
 #Via le nombre de lien
-n = [0.43; 0.76; 0.10; 0.61; 0.21;  0.82; 0.34; 0.14; 0.62; 0.06;  0.42; 0.27; 0.72; 0.38; 0.55];
+n = ones(15,1);
 
 #Matrice bidon (en attendant l'automatisation/google)
-M = zeros(15);
+M = zeros(15); %A remplacer
+%A voir : valeur de 1 si plusieurs fois lien vers même page, ou valeur de nb de fois le lien
 
 disp(M);
-     
+
+#{     
 for i=1:size(M)(1)
   for j=1:size(M)(1) 
     M(j,i) = rand();
   endfor
 endfor
-
+#}
 
 for i=1:size(M)(1)
   somme = sum(M(:,i));
@@ -25,20 +27,20 @@ endfor
 
 
 
-T = 20;
+T = 10;
 
 x = zeros(size(M)(1), T);
 x(:,1) = n; #x initial
 
-for i=2:T
+for i=1:T
     n = M*n;
-    x(:,i) = n;
+    x(:,i+1) = n;
 endfor
 
 figure(1);
-plot(1:T, x(1,:));
+plot(0:T, x(1,:));
 hold on;
 
-for i=2:size(M)(1)
-  plot(1:T, x(i,:));
+for i=1:size(M)(1)
+  plot(0:T, x(i,:));
 endfor
