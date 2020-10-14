d3b734cae9ce1ca31b0d6af52f6da2c76c040902
Arugakente <arugakente@gmail.com>
2020-10-14 16:53:55 +0200

    Enfer sur terre

--- main.m
+++ main.m
@@ -1,32 +1,60 @@
-#Via le nombre de lien
 n = ones(15,1);
 multiCount = true;
 
-#Matrice bidon (en attendant l'automatisation/google)
-files = glob("/Users/guillaume/Documents/Travail/2020-2021/data_science/Projets/DataScienceP1/Dataset/processed/*.txt");
+files = glob("C:/Users/Kente/Documents/DataScienceP1/Dataset/processed/*.txt");
 
 M = zeros(numel(files),numel(files));
 
-for i=1:numel(files)
-  currentFile = textread (files{i}, "%s");
-  linkFrom = str2num(substr(files{i},rindex(files{i},"/")+5,rindex(files{i},".")-(rindex(files{i},"/")+5)));
-  for j =1:numel(currentFile)
-      currentWord = currentFile{j};
-      if  isempty(regexp(currentWord,"linkTo:.*")) == 0
-          linkTo = str2num( substr(currentWord,index(currentWord,":")+5,index(currentWord,".")-(index(currentWord,":")+5)));
-          if linkTo != linkFrom
-                if  multiCount
-                  M(linkFrom,linkTo)+=1;
-                else  
-                  M(linkFrom,linkTo)=1;
-                endif
-           endif
-      endif
+wordList = cell(0,1);
+listSite =  cell(0,1);
 
-      endfor
+for i=1:numel(files)
+  currentFile = textread(files{i}, "%s");
+  linkFrom = str2num(substr(files{i},rindex(files{i},"\\") + 5, rindex(files{i},".") - (rindex(files{i},"\\") + 5)));
+  for j=1:numel(currentFile)
+    currentWord = currentFile{j};
+    if isempty(regexp(currentWord,"linkTo:.*")) == 0 %On traite le lien
+      linkTo = str2num(substr(currentWord,index(currentWord,":")+5,index(currentWord,".")-(index(currentWord,":")+5)));
+      if linkTo != linkFrom
+        if multiCount
+          M(linkFrom,linkTo)+=1;
+        else  
+          M(linkFrom,linkTo)=1;
+        endif
+      endif
+    else %On traite le mot
+      if size(currentWord) != 0
+        %nettoyage des mots
+        
+        isContained = false;
+        for i=1:size(wordList)
+          if size(wordList)!=0 && strcmp(wordList(i),currentWord) %On rajoute le lien associé
+            currentParc = 1;
+            found = false;
+            while !found && currentParc <= size(listSite{i})
+              if(size(listSite{i}) == 1)
+                found = strcmp(listSite{i},linkFrom);
+              else
+                found = strcmp(listSite(i)(currentParc),linkFrom);
+              endif
+              currentParc+=1;
+            endwhile;
+            if !found
+              listSite{i} = [listSite{i}; linkFrom];
+              isContained = true;
+            endif;
+            break;
+          endif
+        endfor 
+        if !isContained %On rajoute le mot à la liste
+          wordList = [wordList;[currentWord]];
+          listSite = [listSite; num2cell([linkFrom])];
+        endif
+      endif
+    endif
+  endfor
 endfor
 
-%A voir : valeur de 1 si plusieurs fois lien vers mï¿½me page, ou valeur de nb de fois le lien
 
 disp(M);
 
@@ -45,7 +73,7 @@ disp(M);
 T = 10;
 
 x = zeros(size(M)(1), T);
-x(:,1) = n; #x initial
+x(:,1) = n;
 
 for i=1:T
     n = M*n;
