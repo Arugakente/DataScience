ce8c65d5be4fb0963f1b4cda10f6d48b21e0630e
Guillaume Gosset <gossetg91@gmail.com>
2020-11-01 20:54:18 +0100

    Auto stash before merge of "master" and "origin/master"

--- Projet2/main2.m
+++ Projet2/main2.m
@@ -9,15 +9,15 @@ txt = textread(strcat(workspace,"/Dataset/dataset.csv"),"%s"); %for titles
 
 N = csv(2:size(csv)(1), 2:size(csv)(2)); %without columns and rows titles
 
-rows = cell(size(N)(1),1);
-columns = cell(size(N)(2),1);
+lines = cell(size(N)(1),1);
+cols = cell(size(N)(2),1);
 
-columns = ostrsplit(txt(1){:}, ",")(2:size(N)(2)+1); %without row x column title
+cols = ostrsplit(txt(1){:}, ",")(2:size(N)(2)+1); %without row x column title
 for i=2:size(txt)
-  rows(i-1) = ostrsplit(txt(i){:}, ",")(1); %only take the row title
+  lines(i-1) = ostrsplit(txt(i){:}, ",")(1); %only take the row title
 endfor
 
-%disp(columns);
+%disp(cols);
 
 
 %Matrix processing
@@ -45,3 +45,52 @@ X = (N-U) ./ sqrt(U); %X matrix for ACP computing
 
 
 %ACP
+
+disp("ACP DEBUG HERE")
+
+#for easier debug :
+#X = X(:,[1,2,3,42]);
+#disp(X);
+
+#calcul de la matrice variance/covariance:
+moy = mean(X,1);
+
+V=cov(X);
+disp(V);
+
+Etype = std(X,0,1);
+
+figure(1)
+scatter(X(:,1),X(:,2))
+figure(2)
+scatter(X(:,3),X(:,4))
+
+for i=1:columns(X)
+   X(:,i)=(X(:,i)-moy(i))/Etype(i);
+endfor
+
+#verification de la standardisation :
+#calcul de la matrice variance/covariance:
+V = cov(X);
+#disp("std dev");
+#disp(std(X));
+#disp("avg");
+#disp(mean(X));
+#disp("X standardized");
+#disp(X);
+
+figure(3)
+scatter(X(:,1),X(:,2))
+figure(4)
+scatter(X(:,3),X(:,4))
+
+[E,D] = eig(V);
+
+infoTot = sum(diag(D));
+#disp(diag(D));
+percentInfo = (diag(D)/infoTot)*100;
+#disp(sum(percentInfo));
+
+figure(5);
+bar(percentInfo);
+#disp(cols);
